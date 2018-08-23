fd0ab04b7e37682d8e1015c18a4261eb9ae1c6de
mohammadkhalaji <mohammadkhalaji76@gmail.com>
2018-08-23 23:03:34 +0430

    readfile and plotpoints implemented

---
+++ ex2/ex2-NotRegularized.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plot
+
+
+def main():
+    file_name = 'ex2data1.txt'
+    X, y, m, n = read_file(file_name, ',')
+    print(X)
+    print(y)
+    plot_points(X, y)
+    plot.show()
+
+
+
+def read_file(file_name, delimiter):
+    points = np.loadtxt(file_name, delimiter=delimiter)
+    x0s = np.ones((points.shape[0], 1))
+    points = np.append(x0s, points, axis=1)
+    m = points.shape[0]
+    n = points.shape[1] - 1
+    return points[..., 0:n], points[..., n:], m, n
+
+
+def plot_points(X, y):
+    z_x = []
+    z_y = []
+    o_x = []
+    o_y = []
+    for i in range(0, X.shape[0]):
+        if y[i][0] > 0.5:
+            o_x.append(X[i][1])
+            o_y.append(X[i][2])
+        else:
+            z_x.append(X[i][1])
+            z_y.append(X[i][2])
+    plot.scatter(o_x, o_y, marker='+', c='g')
+    plot.scatter(z_x, z_y, marker='.', c='r')
+
+
+if __name__ == '__main__':
+    main()
