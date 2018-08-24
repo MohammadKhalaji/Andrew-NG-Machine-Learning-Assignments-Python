aab4ea2b0ca8fd2d3fce3e36659bdfa8f2ca02dd
mohammadkhalaji <mohammadkhalaji76@gmail.com>
2018-08-24 14:09:33 +0430

    updated featuremap

--- ex2/ex2-Regularized.py
+++ ex2/ex2-Regularized.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plot
-from scipy.optimize import minimize
+from scipy.optimize import *
 
 
 def main():
@@ -8,6 +8,20 @@ def main():
     X, y, m, n = read_file(file_name, ',')
     plot_points(X, y)
     X, n = map_features(X)
+    theta = np.zeros((n, 1))
+
+
+    # overfit
+    ans1 = minimize(lambda t: cost(X, y, t, lmd=0), theta, method='Nelder-Mead').x.reshape((n, 1))
+
+    # fine
+    ans2 = minimize(lambda t: cost(X, y, t, lmd=1), theta, method='Nelder-Mead').x.reshape((n, 1))
+
+    #underfit
+    ans3 = minimize(lambda t: cost(X, y, t, lmd=100), theta, method='Nelder-Mead').x.reshape((n, 1))
+
+    print(h(np.array([0, 0, 0.2]).T, ans2))
+
 
 
 def read_file(file_name, delimiter):
@@ -35,6 +49,7 @@ def plot_points(X, y):
     plot.scatter(z_x, z_y, marker='.', c='r')
 
 
+
 def map_features(X):
     deg = 7
     k = 0
@@ -42,13 +57,39 @@ def map_features(X):
     cur = []
     m = X.shape[0]
     for k in range(0, m):
-        for i in range(0, deg):
-            for j in range(0, i+1):
-                cur.append(np.power(X[k][0], i-j) * np.power(X[k][1], j))
-        newX.append(cur)
-        cur = []
+        newX.append(map_feature(X[k]))
     X = np.array(newX)
     return X, X.shape[1]
+
+
+def map_feature(x):
+    deg = 7
+    cur = []
+    for i in range(0, deg):
+        for j in range(0, i+1):
+            cur.append(np.power(x[0], i-j) * np.power(x[1], j))
+    return cur
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def h(X, theta):
+    return sigmoid(np.matmul(X, theta))
+
+
+def cost(X, y, theta, lmd):
+    n = X.shape[1]
+    m = X.shape[0]
+    theta = theta.reshape((n, 1))
+    # we do not penalize theta0
+    penalize = np.matmul(theta.T, theta) - theta[0][0]**2
+    ans = np.matmul(y.T, np.log(h(X, theta))) + np.matmul((1 - y).T, np.log(1 - h(X, theta)))
+    ans = ans * (-1 / m)
+    ans += (lmd / (2 * m)) * penalize
+    return ans
+
 
 
 if __name__ == '__main__':
