5ab24b285f7068979d6f3d4e6fbb0b4a08794ffb
mohammadkhalaji <mohammadkhalaji76@gmail.com>
2018-08-24 19:36:19 +0430

    ex2sklearn plot DONE

--- ex2/ex2-sklearn.py
+++ ex2/ex2-sklearn.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plot
 
 
 def main():
@@ -19,7 +20,8 @@ def main():
         n = X.shape[1]
     regressor = LogisticRegression()
     regressor.fit(X, ny)
-
+    plot_data(y, regressor, originalX, P)
+    plot.show()
     while True:
         inp = input('Enter x to predict value, split by comma:\n')
         inp = inp.replace(' ', '')
@@ -39,6 +41,42 @@ def read_file(file_name, delimiter):
     m = points.shape[0]
     n = points.shape[1] - 1
     return points[..., 0:n], points[..., n:], m, n
+
+
+def plot_data(inpy, regressor, originalX, P):
+    # Build the grid to plot:
+    x_min, x_max = originalX[..., 0].min() - 0.25, originalX[..., 0].max() + 0.25
+    y_min, y_max = originalX[..., 1].min() - 0.25, originalX[..., 1].max() + 0.25
+    x = np.arange(x_min, x_max, 0.2)
+    y = np.arange(y_min, y_max, 0.2)
+    xx, yy = np.meshgrid(x, y)
+
+    # Build the data:
+    zz = np.zeros(xx.shape)
+    for i in range(0, xx.shape[0]):
+        for j in range(0, xx.shape[1]):
+            my_x = np.array([[xx[i][j], yy[i][j]]])
+            if P:
+                my_x = PolynomialFeatures(degree=6).fit_transform(my_x)
+            zz[i][j] = regressor.predict(my_x)
+
+    plot.pcolormesh(xx, yy, zz)
+    # plot points:
+    z_x = []
+    z_y = []
+    o_x = []
+    o_y = []
+    for i in range(0, originalX.shape[0]):
+        if inpy[i][0] == 1:
+            o_x.append(originalX[i][0])
+            o_y.append(originalX[i][1])
+        else:
+            z_x.append(originalX[i][0])
+            z_y.append(originalX[i][1])
+    plot.scatter(o_x, o_y, marker='+', c='g', label='y = 1')
+    plot.scatter(z_x, z_y, marker='.', c='r', label='y = 0')
+
+
 
 
 if __name__ == '__main__':
